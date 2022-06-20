dbf700297842d83645b4d9879eb7bcaadd3341fd
li_huei <hccpaps40832@gmail.com>
2022-06-20 14:53:56 +0800

    commit

--- todo.js
+++ todo.js
@@ -2,8 +2,8 @@ angular.module('todoApp', [])
   .controller('TodoListController', function() {
     var todoList = this;
     todoList.todos = [
-      {text:'learn AngularJS', done:true},
-      {text:'build an AngularJS app', done:false}];
+      {text:'typeA', done:false , sub:[{text:'A1'},{text:'A2'}]},
+      {text:'typeB', done:false , sub:[{text:'B1'},{text:'B2'}]}];
 
     todoList.addTodo = function() {
       todoList.todos.push({text:todoList.todoText, done:false});
